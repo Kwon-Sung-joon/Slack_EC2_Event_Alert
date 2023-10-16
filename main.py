fbdcd6aebf0afb8dd4c5cb4a626924bd658fd2ba
SungJoon Kwon <43159901+Kwon-Sung-joon@users.noreply.github.com>
2023-10-16 15:24:30 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,178 @@
+import json
+import boto3
+
+
+SESSION_KEY={
+    "aws_access_key_id":"",
+        "aws_secret_access_key":"",
+        "aws_session_token":""
+    
+}
+
+class GetEc2Info:
+    def __init__(self,accountId):
+        self.sts_client=boto3.client('sts');
+
+        #get session to target aws account.
+        response = self.sts_client.assume_role(
+            RoleArn=get_ssm_parameters_role(accountId),
+            RoleSessionName="temp-session"
+            )
+        #set aws access config
+        SESSION_KEY["aws_access_key_id"]=response['Credentials']['AccessKeyId']
+        SESSION_KEY["aws_secret_access_key"]=response['Credentials']['SecretAccessKey']
+        SESSION_KEY["aws_session_token"]=response['Credentials']['SessionToken']
+        
+    def get_ec2_name(self,instanceId):
+        #get target instance tags (Alarm tags)
+        ec2_name=instanceId
+        ec2_client=boto3.client('ec2',  aws_access_key_id=SESSION_KEY["aws_access_key_id"],
+        aws_secret_access_key=SESSION_KEY["aws_secret_access_key"],
+        aws_session_token=SESSION_KEY["aws_session_token"]
+        )
+
+        #ec2_client=boto3.client('ec2');
+        ec2_info=ec2_client.describe_instances(InstanceIds=[instanceId])
+        for tags in ec2_info['Reservations'][0]['Instances'][0]['Tags']:
+            if tags['Key'] == 'Name':
+                ec2_name=tags['Value']
+
+        return ec2_name
+
+    def get_ec2_type(self,instanceId):
+        #get target instance tags (Alarm tags)
+        ec2_name=instanceId
+        ec2_client=boto3.client('ec2',  aws_access_key_id=SESSION_KEY["aws_access_key_id"],
+        aws_secret_access_key=SESSION_KEY["aws_secret_access_key"],
+        aws_session_token=SESSION_KEY["aws_session_token"]
+        )
+
+        #ec2_client=boto3.client('ec2');
+        ec2_info=ec2_client.describe_instances(InstanceIds=[instanceId])
+        return ec2_info['Reservations'][0]['Instances'][0]['InstanceType']
+        
+
+def get_ssm_parameters_role(accountId):
+    ssm_client = boto3.client('ssm');
+    chnl_name=ssm_client.get_parameters(Names=['CW_IAM_ROLE_ARN'])['Parameters'];
+    value=chnl_name[0]['Value']
+    # using json.loads()
+    # convert dictionary string to dictionary
+    res = json.loads(value)
+    print("IAM_ROLE_ARN : "+res[accountId])
+    return res[accountId]
+
+
+def get_ssm_parameters(accountId):
+    ssm_client = boto3.client('ssm');
+    svc_name=ssm_client.get_parameters(Names=['SERVICE_NAME'])['Parameters'];
+    value=svc_name[0]['Value']
+    res = json.loads(value)
+    
+    return res[accountId]
+
+def runEventMsg(detail):
+    
+    ec2_client=GetEc2Info(detail['userIdentity']['accountId']);
+    user=detail['userIdentity']['arn'].split('/')[-1]
+    eventName=detail['eventName']
+    region=detail['awsRegion']
+    client = boto3.client('sns')
+    
+    for i in detail['responseElements']['instancesSet']['items']:
+        ec2_tags=[tag for tag in i['tagSet']['items'] if tag['key'] == 'Name']
+        ec2_name=ec2_tags[0]['value'] if len(ec2_tags) > 0 else "UNKNOWN";
+        
+        msg = {
+    "version": "1.0",
+    "source": "custom",
+    "content": {
+         "description": ":warning: *"+get_ssm_parameters(detail['userIdentity']['accountId'])+
+        "*\n*Event Time*\n"+
+        detail['eventTime'] +
+        "\n*EC2 Name*\n"+
+        ec2_name+
+        "\n*EC2 ID*\n"+
+        i['instanceId']+
+        "\n*EC2 Type*\n"+
+        i['instanceType']+
+        "\n*User name*\n"+
+        user+        
+        "\n*Event Mesage*\n"+
+        eventName
+
+    }
+    }
+
+        response = client.publish(
+        TargetArn="arn:aws:sns:ap-northeast-2:589566835476:SNS_TOPIC_EC2_STATE_EVENT",
+        Message=json.dumps({'default': json.dumps(msg)}),
+        MessageStructure='json'
+        )
+
+    
+    
+    
+    return msg;
+    
+    
+
+def otherEventMsg(detail):
+    
+    ec2_client=GetEc2Info(detail['userIdentity']['accountId']);
+    user=detail['userIdentity']['arn'].split('/')[-1]
+    eventName=detail['eventName']
+    region=detail['awsRegion']
+    client = boto3.client('sns')
+    
+    for i in detail['requestParameters']['instancesSet']['items']:
+        
+        msg = {
+    "version": "1.0",
+    "source": "custom",
+    "content": {
+         "description": ":warning: *"+get_ssm_parameters(detail['userIdentity']['accountId'])+
+        "*\n*Event Time*\n"+
+        detail['eventTime'] +
+        "\n*EC2 Name*\n"+
+        ec2_client.get_ec2_name(i['instanceId'])+
+        "\n*EC2 ID*\n"+
+        i['instanceId']+        
+        "\n*EC2 Type*\n"+
+        ec2_client.get_ec2_type(i['instanceId'])+
+        "\n*User name*\n"+
+        user+        
+        "\n*Event Mesage*\n"+
+        eventName
+    }
+    }
+        
+        response = client.publish(
+        TargetArn="arn:aws:sns:ap-northeast-2:589566835476:SNS_TOPIC_EC2_STATE_EVENT",
+        Message=json.dumps({'default': json.dumps(msg)}),
+        MessageStructure='json'
+        )
+
+    
+    
+    
+    return msg;
+    
+    
+    
+def lambda_handler(event, context):
+    detail=event['detail']
+    
+    msg=runEventMsg(detail) if detail['eventName'] == "RunInstances" else otherEventMsg(detail);
+    
+    
+    print(json.dumps(msg));
+    
+    
+
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
+
